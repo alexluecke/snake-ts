2f8c4941a5f59e01bdacfbd36fcb39deaa2bdc50
Alex Luecke <alex.james.luecke@gmail.com>
2018-06-24 12:41:47 -0700

    fix canvas null checking

--- src/canvas-2d-renderer.ts
+++ src/canvas-2d-renderer.ts
@@ -3,7 +3,7 @@ import { Atom } from "./atom";
 
 export class Canvas2DRenderer implements Renderer {
   private _canvas: HTMLCanvasElement;
-  private _context: CanvasRenderingContext2D | null;
+  private _context: CanvasRenderingContext2D;
   private _size = 30;
 
   constructor() {
@@ -11,7 +11,7 @@ export class Canvas2DRenderer implements Renderer {
     this._canvas = document.createElement('canvas');
     this._canvas.setAttribute('width', String(dimension));
     this._canvas.setAttribute('height', String(dimension));
-    this._context = this._canvas.getContext('2d');
+    this._context = this.getContext(this._canvas);
   }
 
   public init(): void {
@@ -19,21 +19,17 @@ export class Canvas2DRenderer implements Renderer {
   }
 
   public render(buffer: Atom[]): void {
-    if (!this._context) {
-      return;
-    }
-
     this._context.fillStyle = "#282828";
     this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
     buffer.forEach(atom => this._drawAtom(atom));
   }
 
   private _drawAtom(atom: Atom): void {
-    if (!this._context) {
-      return;
-    }
-
     this._context.fillStyle = atom.color;
     this._context.fillRect(atom.coord.x * this._size, atom.coord.y * this._size, this._size, this._size);
+  }
+
+  private getContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+    return canvas.getContext('2d') || new CanvasRenderingContext2D();
   }
 }
